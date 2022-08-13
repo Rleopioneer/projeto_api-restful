9d26172e8090231726350fd3725164e0701f3359
Ramon Leonardo <ramon.c.leo@hotmail.com>
2022-08-13 13:03:40 -0300

    Adicionando middelleware de CORS

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-//const path = require('path')
+const cors = require('cors')
 
 const db = require('./database/db')
 const routes = require('./routes/routes')
@@ -8,6 +8,27 @@ const app = express()
 
 //conexão com o banco de dados
 db.connect()
+
+const allowedOrigins = [
+    'http://127.0.0.1:5500',
+    'http://www.api.com.br',
+]
+
+//habilita CORS
+app.use(cors({
+    //função para avaliar se as origens estão habilitadasa para acessar a API
+    origin: function(origin, callback){
+        let allowed = true
+
+        //Exemplo: mobile app
+        if (!origin) allowed = true
+
+        if (!allowedOrigins.includes(origin)) allowed = false
+
+        callback(null, allowed) //null = mensagem apdrão a ser retornada ao navegador caso allowed seja false 
+    }
+
+}))
 
 //habilita servidor para receber dados via post (json)
 app.use(express.json())
